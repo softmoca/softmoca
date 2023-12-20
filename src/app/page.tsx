fc9ce83d5d83398e90486a1c7b0d6f84d474fce9
softmoca <acyc0909@gmail.com>
2023-12-20 21:28:43 +0900

    Init : Next blong project

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="bg-gray-500">
+      Main
+      <div className="text-5xl">hi</div>
+    </main>
+  );
+}
