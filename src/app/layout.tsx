abe13e0aa99caadc8c258d574aaab4771a3ee694
softmoca <acyc0909@gmail.com>
2023-12-21 17:15:32 +0900

    feat : GA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import Footer from "@/components/Footer";
 import "./globals.css";
 import { Open_Sans } from "next/font/google";
 import Header from "@/components/Header";
+import { Suspense } from "react";
+import Analytics from "@/components/Analytics";
 
 const sans = Open_Sans({ subsets: ["latin"] });
 
@@ -23,6 +25,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={sans.className}>
+      <Suspense>
+        <Analytics />
+      </Suspense>
       <body className="flex flex-col w-full max-w-screen-2xl mx-auto">
         <Header />
         <main className="grow">{children}</main>
