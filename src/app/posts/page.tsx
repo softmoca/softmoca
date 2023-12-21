9d12785c9dd7e9f940cf3d234dc5c5b2d3ccbb91
softmoca <acyc0909@gmail.com>
2023-12-21 12:31:36 +0900

    feat : post with catageroy

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,5 +1,14 @@
-import React from "react";
+import { Metadata } from "next";
+import { getAllPosts } from "../api/posts";
+import FilterablePosts from "@/components/FilterablePosts";
 
-export default function Postspage() {
-  return <div>게시글들</div>;
+export const metadata: Metadata = {
+  title: "All Posts",
+  description: "풀스택 관련 블로그 글",
+};
+
+export default async function PostsPage() {
+  const posts = await getAllPosts();
+  const categories = [...new Set(posts.map((post) => post.category))];
+  return <FilterablePosts posts={posts} categories={categories} />;
 }
