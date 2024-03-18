99791fdb20adafcb1716d952634f90177913a38b
softmoca <acyc0909@gmail.com>
2024-03-18 21:09:01 +0900

    refactory : Home <-> Portfolio

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,13 +4,13 @@ export default function Header() {
   return (
     <header className="flex justify-between items-center p-4">
       <Link href="/">
-        <h1 className="text-4xl font-bold">{"Moca's Blog"}</h1>
+        <h1 className="text-4xl font-bold">{"Moca's Portfolio"}</h1>
       </Link>
       <nav className="flex gap-4 text-3xl text-gray-400 font-semibold ">
-        <Link href="/">Home</Link>
+        {/* <Link href="/">Home</Link>
         <Link href="/about">About</Link>
         <Link href="/posts">Posts</Link>
-        <Link href="/contact">Contact</Link>
+        <Link href="/contact">Contact</Link> */}
       </nav>
     </header>
   );
